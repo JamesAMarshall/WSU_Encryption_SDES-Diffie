f6cdb0b917b6b3392afbf019b0a637ec6f0c2969
JamesAMarshall <jmarshall.bb8@gmail.com>
2019-11-16 22:37:49 +1100

    Added all project files to repo

---
+++ DiffieHellman/DiffieHellman/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiffieHellman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "";
+            ans = Console.ReadLine();
+            while (ans != "exit")
+            {
+                while (ans != "manual") // Run random value alogrithm
+                {
+                    DiffieHellman();
+                    ans = Console.ReadLine();
+                }
+
+                if (ans == "manual") // Run algorithm with manaul input
+                {
+                    Console.Write("Enter p: ");
+                    int p = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter g: ");
+                    int g = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Xa: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Xb: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    DiffieHellman(p, g, a, b);
+                }
+
+                ans = Console.ReadLine();
+            } 
+        }
+        
+        static void DiffieHellman()
+        {
+            Random rand = new Random();
+
+            int p = rand.Next(3, 30);
+            while (!isPrime(p))
+                p = rand.Next(3, 30); // Makes p is a random prime number
+
+            List<int> roots = new List<int>(); // get a list of primitive roots
+            foreach (int r in Roots(p)) // put hashed set of roots into a list to be indexed
+                roots.Add(r);
+
+            int g = roots[rand.Next(0, roots.Count)]; // pick a random root via index
+
+            int alice_private_key = rand.Next(3, 30 / 2); // Get a random Xa
+            int bob_private_key = rand.Next(3, 30 / 2); // Get a random Xb
+
+            int alice_generated_key = (int)Math.Pow(g, alice_private_key) % p; // G ^ Xa mod p
+            int bob_generated_key = (int)Math.Pow(g, bob_private_key) % p; // G ^ Xb mod p
+
+            int alice_generated_secret_key = (int)Math.Pow(bob_generated_key, alice_private_key) % p; // Yb ^ Xa mod p
+            int bob_generated_secret_key = (int)Math.Pow(alice_generated_key, bob_private_key) % p; ; // Ya ^ Xb mod p
+
+            Console.WriteLine("P: " + p);
+            Console.WriteLine("G: " + g);
+            Console.WriteLine("Xa: " + alice_generated_key);
+            Console.WriteLine("Xb: " + bob_generated_key);
+
+            if (alice_generated_secret_key == bob_generated_secret_key)
+                Console.WriteLine("K: " + alice_generated_secret_key);
+            else
+                Console.WriteLine("Keys are not identical");
+        }
+
+        static void DiffieHellman(int _p, int _g, int _a, int _b) // Manual input
+        {
+            Random rand = new Random();
+
+            int p = _p; // Set p
+
+            int g = _g; // Set g
+
+            int alice_private_key = _a; // Set Xa
+            int bob_private_key = _b; // Set Xb
+          
+            int alice_generated_key = (int)Math.Pow(g, alice_private_key) % p; // G ^ Xa mod p
+            int bob_generated_key = (int)Math.Pow(g, bob_private_key) % p; // G ^ Xb mod p
+
+            int alice_generated_secret_key = (int)Math.Pow(bob_generated_key, alice_private_key) % p; // Yb ^ Xa mod p
+            int bob_generated_secret_key = (int)Math.Pow(alice_generated_key, bob_private_key) % p; ; // Ya ^ Xb mod p
+
+            //Console.WriteLine("P: " + p);
+            //Console.WriteLine("G: " + g);
+            //Console.WriteLine("Xa: " + alice_generated_key);
+            //Console.WriteLine("Xb: " + bob_generated_key);
+
+            if (alice_generated_secret_key == bob_generated_secret_key)
+                Console.WriteLine("K: " + alice_generated_secret_key);
+            else
+                Console.WriteLine("Keys are not identical");
+        }
+
+        static HashSet<int> Roots(int p)
+        {
+            HashSet<int> primitiveRoots = new HashSet<int>();
+            if (isPrime(p)) // Check p is prime
+            {
+                int phi = p-1; // if p is prime phi = p-1
+                List<int> coprime_group = Coprimes(phi); // Get a list of coprimes
+    
+                int root = findPrimitive(p); // Find a valid g
+                foreach (int i in coprime_group)
+                {
+                    int primitive = (int)Math.Pow(root, i) % p; // Calculate the remaining primitive roots from powers of g
+                    primitiveRoots.Add(primitive); // Add to set
+                }
+            }
+            return primitiveRoots;
+        }
+
+        static int GCD(int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return GCD(b % a, a); // Greatest Common Denominator
+        }
+
+        static List<int> Coprimes(int n)
+        {
+            List<int> coprimes = new List<int>();
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (GCD(n, i) == 1) // Find Coprimes of n up to n
+                    coprimes.Add(i);
+            }
+
+            return coprimes;
+        }
+
+        public static HashSet<int> PrimeFactors(int n)
+        {
+            HashSet<int> factors = new HashSet<int>();
+            while (n % 2 == 0) // only even prime is 2
+            {
+                factors.Add(2);
+                n /= 2;
+            }
+
+            // only odd prime form here, can loop by 2 at a time
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)// While i divides n, add i and divide n 
+                {
+                    factors.Add(i);
+                    n /= i;
+                }
+            }
+
+            if (n > 2) factors.Add(n); // By this stage n is a factor to add to the list
+
+            return factors;
+        }
+
+        static bool isPrime(int n)
+        {
+            if (n <= 1) return false; // 0 and 1 aren't prime
+            if (n <= 3) return true; // 2 and 3 are prime
+            if (n % 2 == 0) return false; // Even numbers aren't prime
+            if (n % 3 == 0) return false; // Clears multiples of 3
+
+            for (int i = 3; i <= (int)Math.Floor(Math.Sqrt(n)); i += 2)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        static int Power(int x, int y, int p)
+        {
+            int res = 1; // x^1 = 1
+
+            x = x % p; // if x is more than p, x mod p
+
+            while (y > 0)
+            {
+                // If y is odd, multiply x with result 
+                if (y % 2 == 1)
+                    res = (res * x) % p;
+
+                // y must be even
+                y = y >> 1; // y = y/2 
+                x = (x * x) % p;
+            }
+            return res;
+        }
+        
+        static int findPrimitive(int n)
+        {
+            if (isPrime(n) == false) return -1;
+           
+            int phi = n - 1;
+
+            HashSet<int> s = PrimeFactors(phi);
+
+            // Check for every number from 2 to phi 
+            for (int r = 2; r <= phi; r++)
+            {
+                bool flag = false;
+                foreach (int a in s)
+                {
+                    // Check if r^((phi)/primefactors) mod n 
+                    // is 1 or not 
+                    if (Power(r, phi / (a), n) == 1)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                // If there was no power with value 1. 
+                if (flag == false) return r;
+            }
+
+            // If no primitive root found 
+            return -1;
+        }
+    }
+}
---
+++ SDES/SDES/Program.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace SDES
+{
+    class Program
+    {
+        static int[] p8 = { 6, 3, 7, 4, 8, 5, 10, 9 };
+        static int[] p10 = { 3, 5, 2, 7, 4, 10, 1, 9, 8, 6 };
+        static int[] ip = { 2, 6, 3, 1, 4, 8, 5, 7 };
+        static int[] ip_1 = { 4, 1, 3, 5, 7, 2, 8, 6 };
+        static int[] ep = { 4, 1, 2, 3, 2, 3, 4, 1 };
+        static int[] p4 = { 2, 4, 3, 1 };
+
+        static int[][] s0 = {
+            new int[]{ 1, 0, 3, 2 },
+            new int[]{ 3, 2, 1, 0 },
+            new int[]{ 0, 2, 1, 3 },
+            new int[]{ 3, 1, 3, 2 },
+        };
+
+        static int[][] s1 = {
+            new int[]{ 0, 1, 2, 3 },
+            new int[]{ 2, 0, 1, 3 },
+            new int[]{ 3, 0, 1, 0 },
+            new int[]{ 2, 1, 0, 3 },
+        };
+
+        static void Main(string[] args)
+        {
+            //uint[] plaintext = {
+            //    0b00100100,
+            //    0b01110100
+            //};
+
+            //uint tenBitKey = 0b1000100011;
+
+            int i = 1;
+
+            string output = "";
+
+            string input = Console.ReadLine();
+
+            if (input == "Encryption")
+            {
+                Console.Write("Enter 8bit plaintext: ");
+                uint p = Convert.ToUInt32(Console.ReadLine(), 2);
+                Console.Write("Enter 10bit Key: ");
+                uint k = Convert.ToUInt32(Console.ReadLine(), 2);
+
+                output += "C:  " + Convert.ToString(Encryption(p, k),2) + "\n";
+            }
+
+            if (input == "Decryption")
+            {
+                Console.Write("Enter 8bit ciphertext: ");
+                uint c = Convert.ToUInt32(Console.ReadLine(), 2);
+                Console.Write("Enter 10bit Key: ");
+                uint k = Convert.ToUInt32(Console.ReadLine(), 2);
+
+                output += "P:  " + Convert.ToString(Decryption(c, k), 2) + "\n";
+            }
+
+            Console.WriteLine(output);
+
+            return;
+        }
+
+        /*
+         * Generate Keys()
+            * P10   /10
+            * Split /5 /5
+            * Left shift 1  AND Left shift 2
+            * Combine /10       Combine /10
+            * P8 /8             P8 /8
+            * K1                K2
+         * Encryption()
+            * IP
+            * fK K1
+            * SW
+            * fK K2
+            * IP-1
+         * Decryption()
+            * IP
+            * fK K2
+            * SW
+            * fK K1
+            * IP-1
+        */
+
+        static uint Encryption(uint plainText, uint Key)
+        {
+            uint IP = P(plainText, ip, 8); // Permutate plaintext by IP
+            uint leftHalf = IP & 0b11110000; // Mask left half
+            leftHalf >>= 4; // shift to get accurate value
+            uint rightHalf = IP & 0b1111; // mask right half
+
+            uint fk1 = fK(leftHalf, rightHalf, K1(Key)); // Fk function
+            uint fk2 = fK(rightHalf, fk1, K2(Key)); // Fk function
+
+            uint combined = fk2; // Combine Fk back to 8bits
+            combined <<= 4;
+            combined += fk1;
+
+            uint cipherText = P(combined, ip_1, 8); // IP ^-1 Inverse
+
+            return cipherText; // output
+        }
+
+        static uint Decryption(uint cipherText, uint Key)
+        {
+            uint IP = P(cipherText, ip, 8); // Permutate cipher by IP
+            uint leftHalf = IP & 0b11110000;// Mask left half
+            leftHalf >>= 4;// shift to get accurate value
+            uint rightHalf = IP & 0b1111; // mask right half
+
+            uint fk1 = fK(leftHalf, rightHalf, K2(Key)); // Fk function
+            uint fk2 = fK(rightHalf, fk1, K1(Key));// Fk function
+
+            uint combined = fk2; // Combine Fk back to 8 bits
+            combined <<= 4;
+            combined += fk1;  
+
+            uint plainText = P(combined, ip_1, 8); // IP ^ -1 Inverse
+            return plainText; // output
+        }
+
+        static uint fK(uint leftHalf, uint rightHalf, uint K)
+        {
+            uint result = f(rightHalf, K); // F function
+            result = result ^ leftHalf; // XOR
+
+            return result;
+        }
+
+        static uint f(uint input, uint K)
+        {
+            uint result = P(input, ep, 4); // Ep perumation
+
+            result = result ^ K; // XOR
+
+            uint leftHalf_Buf = result & 0b11110000; // Mask left half
+            leftHalf_Buf >>= 4; // shift right
+            uint rightHalf_Buf = result & 0b1111; // Mask right Half
+
+            leftHalf_Buf = S(leftHalf_Buf, s0); // Sbox S0
+            rightHalf_Buf = S(rightHalf_Buf, s1); // Sbos S1
+
+            uint combined = leftHalf_Buf; // Combine back to 4 bits
+            combined <<= 2;
+            combined += rightHalf_Buf;
+
+            result = P(combined, p4, 4); // P4
+
+            return result;
+        }
+
+        static uint S(uint input, int[][] sbox)
+        {
+            uint r1 = input & 0b1000; // Mask left bit
+            r1 >>= 2;
+            uint r2 = input & 0b0001; // Mask right bit
+            uint row = r1 + r2; // Bit 1 and 4 = row
+
+            uint col = input & 0b0110; // Bit 2 and 3 = col
+            col >>= 1;
+
+            uint result = (uint)sbox[row][col]; // S row and col
+            return result;
+        }
+
+        static uint K1(uint tenBitKey)
+        {
+            uint result = 0b0;
+
+            result = P(tenBitKey, p10, 10);/* P10 */
+
+            uint leftHalf = result & 0b1111100000; // Mask Left half
+            leftHalf >>= 5;
+            uint rightHalf = result & 0b11111; // Mask Right half
+
+
+            leftHalf = LS1(leftHalf); // Left shift 1
+            rightHalf = LS1(rightHalf); // Left Shift 1
+
+            uint combined = leftHalf; // Combine
+            combined <<= 5;
+            combined += rightHalf;
+
+            result = P(combined, p8, 10); // P8
+            Console.WriteLine(Convert.ToString(result, 2));
+            return result;
+        }
+
+        static uint K2(uint tenBitKey)
+        {
+            uint result = 0b0;
+
+            result = P(tenBitKey, p10, 10);/* P10 */
+
+            uint leftHalf = result & 0b1111100000; // Mask left half
+            leftHalf >>= 5;
+
+            uint rightHalf = result & 0b11111; // Mask right half
+
+            leftHalf = LS1(leftHalf); // Left shift 1
+            rightHalf = LS1(rightHalf); // Left shift 1
+
+            leftHalf = LS2(leftHalf); // Left shift 2
+            rightHalf = LS2(rightHalf);// Left shift 2
+
+            uint combined = leftHalf; // Combine to 10 bits
+            combined <<= 5;
+            combined += rightHalf;
+
+            result = P(combined, p8, 10); // P8
+            Console.WriteLine(Convert.ToString(result, 2));
+            return result;
+        }
+
+        /* Universal Permiation function for bits
+         * Get the index by masking with powers of 2
+         * Get the value of the position and set permutation index to that value 
+         */
+        static uint P(uint input, int[] permutation, int size)
+        {
+            uint result = 0b0;
+
+            for (int i = 0; i < permutation.Length; i++)
+            {
+                uint value = GetBitIndexValue(input, permutation[i], size);
+                if (value == 1)
+                {
+                    result += (uint)Math.Pow(2, permutation.Length - (i + 1));
+                }
+            }
+
+            return result;
+        }
+        
+        static uint GetBitIndexValue(uint input, int index, int size)
+        {
+            // Get mask for a bit index 2 to the power of (size - index)
+            uint mask = (uint)Math.Pow(2, size - (index));
+
+            // Value = input AND mask
+            uint value = input & (uint)(mask);
+
+            if (value != 0)
+            {
+                value = LeftShift(value, index);
+                value = RightShift(value, size);
+            }
+            return value;
+        }
+
+        static uint LS1(uint input)
+        {
+            uint result = input;
+            uint overflow = result & 0b10000; // Mask left bit
+            overflow >>= 4; // Save left bit as single value
+            result <<= 1; // Left shift 1
+            result = result & 0b011111; // Clear extra left bit
+            result += overflow; // add saved left bit for cyclical shift
+            return result;
+        }
+
+        static uint LS2(uint input)
+        {
+            uint result = input;
+            uint overflow = result & 0b11000; // Mask left 2 bits
+            overflow >>= 3; // Save left bit as single value
+            result <<= 2;// Left shift 2
+            result = result & 0b011111;// Clear extra left bits
+            result += overflow;// add saved left bits for cyclical shift
+            return result;
+        }
+
+        static uint LeftShift(uint input, int amount)
+        {
+            switch (amount)
+            {
+                case 1:
+                    input <<= 1;
+                    break;
+                case 2:
+                    input <<= 2;
+                    break;
+                case 3:
+                    input <<= 3;
+                    break;
+                case 4:
+                    input <<= 4;
+                    break;
+                case 5:
+                    input <<= 5;
+                    break;
+                case 6:
+                    input <<= 6;
+                    break;
+                case 7:
+                    input <<= 7;
+                    break;
+                case 8:
+                    input <<= 8;
+                    break;
+                case 9:
+                    input <<= 9;
+                    break;
+                case 10:
+                    input <<= 10;
+                    break;
+                default:
+                    break;
+            }
+
+            return input;
+        }
+
+        static uint RightShift(uint input, int amount)
+        {
+            switch (amount)
+            {
+                case 1:
+                    input >>= 1;
+                    break;
+                case 2:
+                    input >>= 2;
+                    break;
+                case 3:
+                    input >>= 3;
+                    break;
+                case 4:
+                    input >>= 4;
+                    break;
+                case 5:
+                    input >>= 5;
+                    break;
+                case 6:
+                    input >>= 6;
+                    break;
+                case 7:
+                    input >>= 7;
+                    break;
+                case 8:
+                    input >>= 8;
+                    break;
+                case 9:
+                    input >>= 9;
+                    break;
+                case 10:
+                    input >>= 10;
+                    break;
+                default:
+                    break;
+            }
+
+            return input;
+        }
+    }
+}
